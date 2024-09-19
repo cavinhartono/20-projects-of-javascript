493e6fc66eabb280f45f1b669d635af0ec6eb8b6
Cavin Hartono Putra <77716310+cavinhartono@users.noreply.github.com>
2024-09-19 22:26:13 +0700

    Added Tic-Tac-Toe Game

---
+++ tictactoe/script.js
@@ -0,0 +1,18 @@
+let player = "x",
+  isStart = false,
+  isPause = false;
+
+const WinConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function choosePlayer(choice) {
+  player = player == "x" ? "o" : "x";
+}
